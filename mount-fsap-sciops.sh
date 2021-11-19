011679ac1a5a8160cfd257703e7bff2e8ecddfb9
Drew Yang <drew@vathes.com>
2021-11-19 16:02:17 +0000

    rm mounting .sh

--- mount-fsap-sciops.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Need to build and install: https://docs.aws.amazon.com/efs/latest/ug/overview-amazon-efs-utils.html
-# Remember to run this script with sudo
-mount -t efs -o tls,accesspoint=fsap-0bc63e40c6bffd915 fs-5271d529: /mnt/sciops
