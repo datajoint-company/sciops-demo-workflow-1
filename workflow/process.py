5042d515f2ee86ca4cea944feecd3c65d94c7801
Thinh Nguyen <thinh@vathes.com>
2022-01-24 18:22:13 -0600

    use datajoint-utilities dj_worker

--- workflow/process.py
+++ workflow/process.py
@@ -1,10 +1,13 @@
+import sys
 import time
 import logging
-from workflow.pipeline import session, ephys, db_prefix
-from workflow.djworker import WorkerLog, DataJointWorker
+from datajoint_utilities.dj_worker import WorkerLog, DataJointWorker, parse_args
 
-logger = logging.getLogger(__name__)
-logger.setLevel('INFO')
+from workflow.pipeline import session, ephys, db_prefix
+
+
+_logger = logging.getLogger(__name__)
+_logger.setLevel('INFO')
 
 
 def auto_generate_probe_insertions():
@@ -12,9 +15,9 @@ def auto_generate_probe_insertions():
         try:
             ephys.ProbeInsertion.auto_generate_entries(skey)
         except FileNotFoundError as e:
-            logger.debug(str(e))
+            _logger.debug(str(e))
         except Exception as e:
-            logger.error(str(e))
+            _logger.error(str(e))
 
 
 def auto_generate_clustering_tasks():
@@ -22,28 +25,76 @@ def auto_generate_clustering_tasks():
         try:
             ephys.ClusteringTask.auto_generate_entries(rkey)
         except FileNotFoundError as e:
-            logger.debug(str(e))
+            _logger.debug(str(e))
         except Exception as e:
-            logger.error(str(e))
+            _logger.error(str(e))
 
 
-worker = DataJointWorker('sciops_worker', db_prefix + 'log',
-                         db_prefix=db_prefix,
-                         run_duration=3600*3,
-                         sleep_duration=10)
+# -------- Define worker(s) --------
+worker_schema_name = db_prefix + "workerlog"
+autoclear_error_patterns = ['%FileNotFound%']
 
-worker(auto_generate_probe_insertions)
-worker(ephys.EphysRecording, max_calls=10)
-worker(auto_generate_clustering_tasks)
-worker(ephys.Clustering, max_calls=5)
-worker(ephys.CuratedClustering, max_calls=10)
-worker(ephys.LFP, max_calls=1)
-worker(ephys.WaveformSet, max_calls=1)
+# standard worker for non-GPU jobs
+standard_worker = DataJointWorker('standard_worker',
+                                  worker_schema_name,
+                                  db_prefix=db_prefix,
+                                  run_duration=1,
+                                  sleep_duration=10,
+                                  autoclear_error_patterns=autoclear_error_patterns)
+
+standard_worker(auto_generate_probe_insertions)
+standard_worker(ephys.EphysRecording, max_calls=10)
+standard_worker(auto_generate_clustering_tasks)
+standard_worker(ephys.Clustering, max_calls=5)
+standard_worker(ephys.CuratedClustering, max_calls=10)
+standard_worker(ephys.LFP, max_calls=1)
+standard_worker(ephys.WaveformSet, max_calls=1)
+
+# spike_sorting worker for GPU required jobs
+
+spike_sorting_worker = DataJointWorker('spike_sorting_worker',
+                                       worker_schema_name,
+                                       db_prefix=db_prefix,
+                                       run_duration=1,
+                                       sleep_duration=10,
+                                       autoclear_error_patterns=autoclear_error_patterns)
+
+standard_worker(ephys.Clustering, max_calls=5)
 
 
-def run():
-    worker.run()
+# -------- Run worker(s) --------
+configured_workers = {
+    'standard_worker': standard_worker,
+    'spike_sorting_worker': spike_sorting_worker
+}
+
+
+def run(**kwargs):
+    worker = configured_workers[kwargs["worker_name"]]
+    worker._run_duration = kwargs["duration"]
+    worker._sleep_duration = kwargs["sleep"]
+
+    try:
+        worker.run()
+    except Exception:
+        _logger.exception(
+            "Worker '{}' encountered an exception:".format(kwargs["worker_name"])
+        )
+
+
+def cli():
+    """
+    Calls :func:`run` passing the CLI arguments extracted from `sys.argv`
+
+    This function can be used as entry point to create console scripts with setuptools.
+    """
+    args = parse_args(sys.argv[1:])
+    run(
+        priority=args.priority,
+        duration=args.duration,
+        sleep=args.sleep
+    )
 
 
 if __name__ == '__main__':
-    run()
+    cli()
